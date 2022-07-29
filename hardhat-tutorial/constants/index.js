d420456c9348b4574c086b7ce131b47eb3d7d012
Arunkumar Natarajan <dtsarun@gmail.com>
2022-07-29 15:43:02 +0530

    Initial Coin Offering

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x86458ecAC80dDB9006501352D8bD22Eac872f3c4";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
